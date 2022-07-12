593749bc63d4717e5ed9f9e55605eadbb8648fc8
Jon Manning <jon@secretlab.com.au>
2022-07-12 16:09:58 +1000

    Use ts-loader to import Typescript (instead of manually invoking tsc before webpack)

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,30 @@
 const path = require('path');
 
 module.exports = {
-    entry: {
-        yarnspinner: {
-            "import" : "./src/index.js",
-            "dependOn" : 'data'
-        },
-        "data": './src/data.js'
+  entry: {
+    yarnspinner: {
+      "import": "./src/index.ts",
+      "dependOn": 'data',
     },
+    "data": './src/data.ts'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
   output: {
     filename: '[name].js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    optimization: {
-        minimize: true
-    }
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    minimize: true
+  }
 };
