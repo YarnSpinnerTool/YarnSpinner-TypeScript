a9628f71d6af0772aa168f1142a1c54659dd9574
Jon Manning <jon@secretlab.com.au>
2022-07-15 14:44:13 +1000

    Don't include test data script in exported builds

--- webpack.config.js
+++ webpack.config.js
@@ -12,82 +12,92 @@ const PATHS = {
   src: path.join(__dirname, 'src')
 };
 
-module.exports = {
-  entry: {
-    yarnspinner: {
-      "import": "./src/index.ts",
-      "dependOn": 'data',
-    },
-    "data": './src/data.ts'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.scss$/, // or /\.css$/i if you aren't using sass
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader",
-        ],
-      },
-      {
-        test: /\.svg$/i,
-        type: 'asset/inline',
-    },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  optimization: {
-    minimize: true,
-    splitChunks: {
-      cacheGroups: {
-        styles: {
-          name: 'styles',
-          test: /\.css$/,
-          chunks: 'all',
-          enforce: true
-        }
-      }
-    }
-  },
-  devServer: {
-    static: './dist',
-    watchFiles: ['src/**/*'],
-  },
-  devtool: 'source-map',
-  mode: 'development',
-  performance: {
-    // TODO: The generated artifact is designed to be all-in-one, so while
-    // keeping the file size small is desirable, we'll disable performance hints
-    // for now.
-    hints: false
-  },
-  plugins: [
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
 
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    }),
-    new PurgecssPlugin({
-      paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
-    }),
-    new HTMLInlineCSSWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      inject: true,
-      urlBase: process.env['BASE_URL'] || '',
-    }),
-    new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/yarnspinner/]),
-]
+  return {
+    entry: {
+      ...(isProduction ? {
+        yarnspinner: {
+          "import": "./src/index.ts",
+        },
+      } : {
+        yarnspinner: {
+          "import": "./src/index.ts",
+          "dependOn": 'data',
+        },
+        "data": './src/data.ts'
+      }),
+  },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.scss$/, // or /\.css$/i if you aren't using sass
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader",
+            "sass-loader",
+          ],
+        },
+        {
+          test: /\.svg$/i,
+          type: 'asset/inline',
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    optimization: {
+      minimize: true,
+      splitChunks: {
+        cacheGroups: {
+          styles: {
+            name: 'styles',
+            test: /\.css$/,
+            chunks: 'all',
+            enforce: true
+          }
+        }
+      },
+    },
+    devServer: {
+      static: './dist',
+      watchFiles: ['src/**/*'],
+    },
+    devtool: 'source-map',
+    mode: 'development',
+    performance: {
+      // TODO: The generated artifact is designed to be all-in-one, so while
+      // keeping the file size small is desirable, we'll disable performance hints
+      // for now.
+      hints: false
+    },
+    plugins: [
+
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css"
+      }),
+      new PurgecssPlugin({
+        paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
+      }),
+      new HTMLInlineCSSWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        inject: true,
+        urlBase: process.env['BASE_URL'] || '',
+      }),
+      new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/yarnspinner/]),
+    ]
+  }
 };
