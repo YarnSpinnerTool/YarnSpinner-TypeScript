e0b12c8b8933cde06fac37fae9372b66be8ab12c
Jon Manning <jon@secretlab.com.au>
2024-06-23 17:01:52 +1000

    Recompile demo script for Yarn v3

--- munger.py
+++ munger.py
@@ -1,5 +1,7 @@
 import csv
+import json
 
+# Write string table
 dict = []
 with open('Output.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
@@ -14,6 +16,28 @@ opener = "const stringTable: { [key: string]: string } = {";
 ender = "};"
 print(f'{opener}{output}{ender}')
 
+# Write metadata table
+dict = []
+with open('Output-metadata.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        # id,node,lineNumber,tags
+        entry = {
+            x:row[x] for x in ['id','node','lineNumber']
+        }
+        entry["tags"] = row["tags"].split(" ")
+        text = json.dumps(entry)
+
+        # text = text.replace("\\", "\\\\" );
+        # text = text.replace("\"", "\\\"" );
+        dict.append(f"\"{row['id']}\":{text}")
+
+output = ",".join(dict)
+opener = "const metadataTable: { [key: string]: MetadataEntry } = {";
+ender = "};"
+print(f'{opener}{output}{ender}')
+
+# Write compiled program
 hex = []
 with open("Output.yarnc", "rb") as f:
     while (byte := f.read(1)):
